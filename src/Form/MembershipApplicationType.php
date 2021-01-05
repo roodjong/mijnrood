7cb8266bfd44503222cf5502baad973ee64c1b90
Robin de Rooij <rderooij685@gmail.com>
2021-01-05 17:08:38 +0100

    add SP membership as requirement

--- src/Form/MembershipApplicationType.php
+++ src/Form/MembershipApplicationType.php
@@ -32,6 +32,14 @@ class MembershipApplicationType extends AbstractType
             ->add('address', null, ['label' => 'Adres', 'error_bubbling' => true, 'constraints' => [new NotBlank()]])
             ->add('city', null, ['label' => 'Plaats', 'error_bubbling' => true, 'constraints' => [new NotBlank()]])
             ->add('postCode', null, ['label' => 'Postcode', 'error_bubbling' => true, 'constraints' => [new NotBlank()]])
+            ->add('isSPMember', CheckboxType::class, [
+                'label' => 'Ik ben lid van de SP.',
+                'label_html' => false,
+                'mapped' => false,
+                'required' => true,
+                'error_bubbling' => true,
+                'constraints' => [new IsTrue(['message' => 'Je moet lid zijn van de SP om ook lid te worden van ROOD.'])]
+            ])
             ->add('accept', CheckboxType::class, [
                 'label' => 'Ik heb het <a target="_blank" href="https://roodjongindesp.nl/privacybeleid">privacybeleid</a> gelezen en ik ga daarmee akkoord.',
                 'label_html' => true,
