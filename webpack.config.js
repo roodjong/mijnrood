47c3a0264415c8052489f87e0fb2a82f8f48552f
Jelle Besseling <jelle@pingiun.com>
2021-12-22 14:06:35 +0100

    Use webpack watch in node-service

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath('public/assets/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('/assets')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
