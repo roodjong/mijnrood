efb70f7d22cf715a4d0f3c43274783d5b4695f4e
Robin de Rooij <rderooij685@gmail.com>
2022-03-08 19:46:33 +0100

    Change contribution for socialisten utrecht

--- src/Form/Contribution/ContributionIncomeType.php
+++ src/Form/Contribution/ContributionIncomeType.php
@@ -5,7 +5,8 @@ namespace App\Form\Contribution;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\{ ChoiceType, TextType };
+use Symfony\Component\Form\Extension\Core\Type\{ ChoiceType, MoneyType};
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 use App\Entity\ChosenContribution;
 
 class ContributionIncomeType extends AbstractType
@@ -13,16 +14,16 @@ class ContributionIncomeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('contributionAmount', ChoiceType::class, [
-                'label' => 'Maandinkomen:', 'choices' => [
-                    'Tot en met €2000 (ik betaal €7,50 contributie per kwartaal)' => 750,
-                    '€2000-€3499 (ik betaal €15,00 contributie per kwartaal)' => 1500,
-                    '€3500 en daarboven (ik betaal €22,50 contributie per kwartaal)' => 2250,
-                    'ik betaal een hogere contributie, namelijk:' => 0,
+            ->add('contributionAmount', MoneyType::class, [
+                'label' => 'Contributie:',
+                'divisor' => 100,
+                'constraints' => [
+                    new GreaterThanOrEqual([
+                        'value' => 600,
+                        'message' => 'Er is iets verkeerd gegaan. Heeft u minimaal een contributie hoger dan 6 euro per kwartaal ingesteld?',
+                    ])
                 ],
-                'expanded' => true
-            ])
-            ->add('otherAmount', TextType::class, ['label' => '€', 'required' => false]);
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
