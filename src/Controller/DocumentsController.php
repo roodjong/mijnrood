c7d82a8a6b5934e539d4bf7de6281be38035eb2e
Robin de Rooij <rderooij685@gmail.com>
2023-01-18 21:02:25 +0100

    Documents: sort by name

--- src/Controller/DocumentsController.php
+++ src/Controller/DocumentsController.php
@@ -20,7 +20,7 @@ class DocumentsController extends AbstractController
 
         $repoFolders = $this->getDoctrine()->getRepository(DocumentFolder::class);
         $folders = $repoFolders->findByParent($folder);
-        $documents = $this->getDoctrine()->getRepository(Document::class)->findByFolder($folder);
+        $documents = $this->getDoctrine()->getRepository(Document::class)->findByFolder($folder, ['name' => 'ASC']);
 
         $canCreateFolder = $canUpload = $canDelete = $this->isGranted('ROLE_ADMIN');
 
