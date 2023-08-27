0cbc2ae90c5dc29ac6a32f3163db459be676153f
Jelle Besseling <jelle@pingiun.com>
2023-08-27 19:03:36 +0200

    Don't download html files as attachments

--- src/Controller/DocumentsController.php
+++ src/Controller/DocumentsController.php
@@ -57,8 +57,10 @@ class DocumentsController extends AbstractController
         }
 
         $response = new Response();
-        $response->headers->set('Content-Disposition', 'attachment; filename="'.urlencode($document->getFileName()).'"');
-        $response->headers->set('Content-Type', 'application/octet-stream');
+        if (!str_ends_with($document->getFileName(), '.html') {
+            $response->headers->set('Content-Disposition', 'attachment; filename="'.urlencode($document->getFileName()).'"');
+            $response->headers->set('Content-Type', 'application/octet-stream');
+        }
         $response->setContent(file_get_contents($this->getParameter('documents_directory').'/'.$document->getUploadFileName()));
         return $response;
     }
